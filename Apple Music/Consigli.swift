ea712e3343de75ace79c0d1a8e3ca41ff49e9ec4
SalvatoreDoc <capassosalvatore2001@gmail.com>
2023-11-21 13:58:25 +0100

    si

--- Apple Music/Consigli.swift
+++ Apple Music/Consigli.swift
@@ -10,11 +10,11 @@ import Foundation
 class ConsigliViewModel {
 
     var Carte = [
-        Card(AlbumImage: "Geolier", NomeArtista: "Geolier", AlbumName: "Emanuele", consigli: "Per te", categoriacolore:.red),
-        Card(AlbumImage: "Marra1", NomeArtista: "Marracash", AlbumName: "NOI, LORO, GLI ALTRI", consigli: "Per te", categoriacolore:.green),
-        Card(AlbumImage: "Marra2", NomeArtista: "Marracash", AlbumName: "Persona", consigli: "Altro da", categoriacolore:.black),
-        Card(AlbumImage: "Sfera", NomeArtista: "Sfera Ebbasta", AlbumName: "Famoso",consigli: "Aggiunti di recente", categoriacolore:.gray),
-        Card(AlbumImage: "mercury", NomeArtista: "Imagen Dragons", AlbumName: "Mercury - Acts 1 & 2", consigli: "Altro da", categoriacolore:.red),
-        Card(AlbumImage: "scaled & icy", NomeArtista: "Twenty one pilote", AlbumName: "Scaled & icy",consigli: "Aggiunti di recente", categoriacolore:.pink)
+        Card(AlbumImage: "Geolier", NomeArtista: "Geolier", AlbumName: "Emanuele", consigli: "For you", categoriacolore:.red),
+        Card(AlbumImage: "Marra1", NomeArtista: "Marracash", AlbumName: "NOI, LORO, GLI ALTRI", consigli: "New Release", categoriacolore:.green),
+        Card(AlbumImage: "Marra2", NomeArtista: "Marracash", AlbumName: "Persona", consigli: "For you", categoriacolore:.black),
+        Card(AlbumImage: "Sfera", NomeArtista: "Sfera Ebbasta", AlbumName: "Famoso",consigli: "Made for you", categoriacolore:.gray),
+        Card(AlbumImage: "mercury", NomeArtista: "Imagen Dragons", AlbumName: "Mercury - Acts 1 & 2", consigli: "More from Imagen Dragons", categoriacolore:.red),
+        Card(AlbumImage: "scaled & icy", NomeArtista: "Twenty one pilote", AlbumName: "Scaled & icy",consigli: "For you", categoriacolore:.pink)
 ]
 }
--- Apple Music/ContentView.swift
+++ Apple Music/ContentView.swift
@@ -11,98 +11,93 @@ struct ContentView: View {
     
     var body: some View {
         
-            
-             
+        
+        
+        
+        NavigationStack {
+            ScrollView{
                 
-                NavigationStack {
-                    ScrollView{
+        
                     
-                    VStack{
-                        
-                        VStack(alignment: .leading, spacing: 0) {
-                            HStack {
+                VStack(alignment:.leading){
+                    Text("Top Picks")
+                        .bold()
+                        .font(.title2)
+                    
+                    
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHGrid(rows: [GridItem(.fixed(360))], spacing: 7) {
+                            ForEach(ConsigliViewModel().Carte) { card in
+                                CreazioneCard(card: card)
                                 
-                                Text("Ascolta ora")
-                                    .font(.largeTitle)
-                                    .bold()
-                                    .foregroundColor(.black)
-                                    .offset(y:20)
-                                
-                                Spacer()
-                                Image(systemName: "person.crop.circle")
-                                    .resizable()
-                                    .frame(width: 35, height: 35)
-                                    .foregroundColor(.red)
-                                    .offset(y:20)
                             }
                             
-                            
-                            Text("In primo piano")
-                                .foregroundColor(.black)
-                                .bold()
-                                .font(.system(size: 23))
-                                .offset(y:40)
-                            
-                            
                         }
-                        
-                        
-                        Spacer()
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            LazyHGrid(rows: [GridItem(.fixed(360))], spacing: 7) {
-                                ForEach(ConsigliViewModel().Carte) { card in
-                                    CreazioneCard(card: card)
-                                    
-                                }
+                    }
+                }
+                VStack(alignment:.leading){
+                    Text("Recently Playes")
+                        .foregroundColor(.black)
+                        .bold()
+                        .font(.title2)
+                    
+                    
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHGrid(rows: [GridItem(.fixed(200))], spacing: 7) {
+                            ForEach(CardViewModel().Cards) { card in
+                                CardView(card: card)
+                                
+                                
+                                
                                 
                             }
                         }
+                    }
+                    }
+                
+                
+            }.listStyle(.plain)
+                .navigationTitle("Listen now")
+                
+                .padding()
+            
+            
+            Spacer()
+                .overlay{
+                    
+                    NavigationLink(destination:Apple_Music.Prova()){
+                        Bottone()
+                            .padding(.bottom, 50.0)
                         
-                        Text("Ascoltati di recente")
-                            .foregroundColor(.black)
-                            .bold()
-                            .font(.title2).offset(x:-80)
-                        
-                        
-                        
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            LazyHGrid(rows: [GridItem(.fixed(200))], spacing: 7) {
-                                ForEach(CardViewModel().Cards) { card in
-                                    CardView(card: card)
-                                    
-                                    
-                                }
-                                
-                            }
-                        }
                         
                     }
-                    .navigationBarTitleDisplayMode(.inline)
-                    .padding()
-                    
-                    
-                    
                 }
-                
-                Spacer()
-                        .overlay{
-                            
-                            NavigationLink(destination:Apple_Music.Prova()){
-                                Bottone()
-                                    .padding(.bottom, 50.0)
-                                
-                                
-                            }
-                        }
-            }
-                
-                
-                
+                .toolbar(content: {
+                   
+                    ToolbarItem(placement:.navigationBarTrailing)
+                    {
+                        Image(systemName: "person.crop.circle")
+                            .resizable()
+                            .frame(width: 35, height: 35)
+                            .padding()
+                            .foregroundColor(.red)
+                        
+                        
+                    }
+                })
         }
+        
+        
         
     }
     
+    
 
+        }
+        
+    
 
 #Preview {
     ContentView()

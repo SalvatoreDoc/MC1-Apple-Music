53c24958218fda42dd9062c2a6aea742a5d956cc
SalvatoreDoc <capassosalvatore2001@gmail.com>
2023-11-22 09:50:20 +0100

    Fine

--- Apple Music/CreazioneCard.swift
+++ Apple Music/CreazioneCard.swift
@@ -10,53 +10,55 @@ import SwiftUI
 struct CreazioneCard: View {
     let card : Card
     var body: some View {
-   
-            VStack(spacing: 0){
-               
+        NavigationStack{
+            
+                
                 Text(card.consigli)
                     .offset(x:-100,y:20)
-                        .font(.system(size: 15))
-                        .frame(height: 70)
-                       .frame(width: 100)
-                        .foregroundColor(.black)
-                        .opacity(0.4)
-                        .padding(6)
+                    .font(.system(size: 15))
+                    .frame(height: 70)
+                    .frame(width: 100)
+                    .foregroundColor(.black)
+                    .opacity(0.4)
+                    .padding(6)
                 
                 
-                        
+                
                 VStack(spacing: 0){
-                Image(card.AlbumImage)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 250)
-                    .frame(height: 250)
-                  
-                
+                    Image(card.AlbumImage)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 250)
+                        .frame(height: 250)
                     
-                ZStack{
-                    card.categoriacolore
-                        .opacity(0.8)
-                      
-                        .frame(width: 250, height: 80)
-              
+                    
+                    
+                    ZStack{
+                        card.categoriacolore
+                            .opacity(0.8)
                         
-                    VStack{
-                        Text(card.AlbumName)
-                            .font(.system(size: 15))
-                            .foregroundColor(.white)
-                            .bold()
-                            .frame(height: 10)
-                            .frame(width: 100)
+                            .frame(width: 250, height: 80)
                         
-                        Text(card.NomeArtista)
-                            .font(.system(size: 13))
-                            .frame(height: 2)
-                            .frame(width: 100)
-                            .foregroundColor(.white)
-                            .padding(2)
+                        
+                        VStack{
+                            Text(card.AlbumName)
+                                .font(.system(size: 15))
+                                .foregroundColor(.white)
+                                .bold()
+                                .frame(height: 10)
+                                .frame(width: 100)
+                            
+                            Text(card.NomeArtista)
+                                .font(.system(size: 13))
+                                .frame(height: 2)
+                                .frame(width: 100)
+                                .foregroundColor(.white)
+                                .padding(2)
+                        }
                     }
-                }
-            }.cornerRadius(20)
+                }.cornerRadius(20)
+                
+            
         }
     }
 }
